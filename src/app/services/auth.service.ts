e63ff3260718c5fbfb8b1caec0b551b5e88c06f7
SuchitaGhadge <suchita8494.sg@gmail.com>
2022-02-01 00:41:48 +0530

    firebase auth service created

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+// import firebase from 'firebase/compat/app';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
+
+  signup(email:string, password:string){
+    return this.auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  signin(email:string, password:string){
+    return this.auth.createUserWithEmailAndPassword(email, password)
+    // this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  getUser(){
+    return this.auth.authState;
+  }
+
+  signOut(){
+    return this.auth.signOut();
+  }
 }
