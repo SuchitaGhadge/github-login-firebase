370cfea14d2faeed41408b9f029fc491eb29889a
SuchitaGhadge <suchita8494.sg@gmail.com>
2022-02-01 11:06:41 +0530

    header.ts signout function

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  email:any = null;
+  constructor(private auth : AuthService, private router : Router, private toastr: ToastrService) {
+    auth.getUser().subscribe((user) => {
+      this.email = user?.email;
+    })
+   }
 
   ngOnInit(): void {
   }
 
+  async handleSignOut(){
+    try{
+      const res = await this.auth.signOut();
+      this.router.navigateByUrl("./signin")
+      this.toastr.info("Login again to continue")
+      this.email = null;
+    }catch(error){
+      this.toastr.error('Something went Wrong!')
+    }
+  }
 }
