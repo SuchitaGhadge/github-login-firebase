845a9f21ae5af0c6b3036aef582bcc816ff50e24
SuchitaGhadge <suchita8494.sg@gmail.com>
2022-02-01 21:33:03 +0530

    repo component

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ChangeDetectorRef } from '@angular/core';
+import { GithubService } from 'src/app/services/github.service';
 
 @Component({
   selector: 'app-repos',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repos.component.css']
 })
 export class ReposComponent implements OnInit {
+  @Input() repoUrl:string | undefined;
+  repos:any = []
 
-  constructor() { }
+  constructor(private githubServices: GithubService, private ref: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void{
+    if(this.repoUrl){
+      this.githubServices.getRepos(this.repoUrl).subscribe(
+        (repos:any) => {
+          this.repos = repos;
+
+          this.ref.detectChanges();
+        },
+        (error) => {
+          console.log("Error...", error)
+        }
+      )
+    }else{
+
+    }
   }
 
 }
