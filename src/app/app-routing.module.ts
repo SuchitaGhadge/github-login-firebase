fa0a4d6ff53e00060db067090dc276d84b884350
SuchitaGhadge <suchita8494.sg@gmail.com>
2022-02-02 00:55:04 +0530

    app component and routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// pages
+import { HomeComponent } from './pages/home/home.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+// guarding
+import { AngularFireAuthGuard, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+const redirectUnauthorizedToSignin = () => redirectUnauthorizedTo(['signin']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['']);
+
+const routes: Routes = [
+  { 
+    path: 'signin', 
+    component: SigninComponent,        
+    canActivate: [AngularFireAuthGuard], 
+    data: { authGuardPipe: redirectLoggedInToHome },
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { autGuardPipe: redirectUnauthorizedToSignin}
+  },
+  {
+    path: "**",
+    component: PagenotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
